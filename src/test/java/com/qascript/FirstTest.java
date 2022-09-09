7324e46fe8d5ab59e254f13bf2d1ac1aad14d9ef
Tapan221 <tapanachary.44@gmail.com>
2022-09-09 14:04:13 +0530

    headless

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -14,15 +14,8 @@ public class FirstTest {
         WebDriver driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless","no-sandbox");
-		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-		options.addArguments("start-maximized"); // open Browser in maximized mode
-		options.addArguments("disable-infobars"); // disabling infobars
-		options.addArguments("--disable-extensions"); // disabling extensions
-		options.addArguments("--disable-gpu"); // applicable to windows os only
-		 // Bypass OS security model
-		options.addArguments("--remote-debugging-port=9222");
-		options.addArguments("--window-size=1920x1080");
+        options.addArguments("headless");
+		options.addArguments("--disable-gpu"); 
 		driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());

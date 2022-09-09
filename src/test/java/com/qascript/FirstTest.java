bf016dcdffe27e92c5ad15d0757cba05fdc8827d
Tapan221 <tapanachary.44@gmail.com>
2022-09-09 15:54:06 +0530

    assert

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -1,28 +1,25 @@
 package com.qascript;
 
 import org.junit.Test;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class FirstTest {
 
-
-    @Test
-    public void OpenBrowser()  {
-        WebDriver driver ;
-        
-       // System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
+	@Test
+	public void OpenBrowser() {
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("headless");
 		options.addArguments("disable-gpu");
 		driver = new ChromeDriver(options);
-        driver.get("https://www.google.com");
-        System.out.println("Title of the page is: " + driver.getTitle());
-       
-}
-    
-}
+		driver.get("https://www.google.com");
+		System.out.println("Title of the page is: " + driver.getTitle());
+		Assert.assertTrue("Page title is not correct", driver.getTitle().equals("Google"));
 
+	}
 
+}
